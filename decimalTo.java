4eab56137e5a39f09db4d975704f81e1f476e5c9
Saurabh Kumar <saurabhpower2@gmail.com>
2026-05-29 16:03:48 +0530

    Update and rename MdecimalToO.java to decimalTo.java

--- decimalTo.java
+++ decimalTo.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MdecimalToO {
+public class decimalTo {
     static void main(String[] args) {
         int  result = 0,rem = 0,rev = 0,decimal = 0;
         System.out.print("Enter the number :");
